129df13f43432b8d1ca8e5590fcd661f4f128b6a
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2025-06-15 15:33:53 +0530

    Time: 0 ms (100%), Space: 46.7 MB (29.01%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int l = 0;
+        int r = n-1;
+        int lmax = 0,rmax = 0,total = 0;
+        while(l<r){
+            if(height[l] <= height[r]){
+                if(lmax > height[l]){
+                    total += lmax-height[l];
+                }else{
+                    lmax = height[l];
+                }
+                l++;
+            }else{
+                if(rmax > height[r]){
+                    total += rmax - height[r];
+                }else{
+                    rmax = height[r];
+                }
+                r--;
+            }
+        }
+        return total;
+        
+    }
+}
