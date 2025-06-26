6853d84164d7663eaad0040bcaf1eb191e321d8d
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2025-06-26 11:13:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,22 +10,23 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<Integer> minheap = new PriorityQueue<>();
+        PriorityQueue<Integer> minheap = new PriorityQueue<>((a,b)->a.val-b.val);
         for(ListNode head : lists){
-            ListNode curr = head;
-            while(curr != null){
-                minheap.add(curr.val);
-                curr = curr.next;
+            if(head != null){
+                minheap.offer(head);
+            }     
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+        while(!minheap.isEmpty()){
+            ListNode node = minheap.poll();
+            tail.next = node;
+            tail=tail.next;
+            while(node.next != null){
+                minheap.offer(node.next);
             }
         }
-        ListNode ans = new ListNode(-1);
-        ListNode tail = ans;
-        while(!minheap.isEmpty()){
-            int ele = minheap.poll();
-            tail.next = new ListNode(ele);
-            tail = tail.next;
-        }
-        return ans.next;
+        return dummy.next;
         
     }
 }
