12c6fd887fb6292832a9321fa40b1c71cc289758
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2025-07-23 21:34:30 +0530

    Time: 0 ms (100%), Space: 41.3 MB (37.76%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,38 +1,30 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n==1) return nums[0];
-        List<Integer> arr1 = new ArrayList<>();
-        List<Integer> arr2 = new ArrayList<>();
-        for(int i = 0;i<n;i++){
-            if(i != n-1){
-                arr1.add(nums[i]);
-            }
-            if(i != 0){
-                arr2.add(nums[i]);
-            }
-        }
-        int[] arr11 = arr1.stream().mapToInt(i -> i).toArray();
-        int[] arr22 = arr2.stream().mapToInt(i -> i).toArray();
-        int[] dp1 = new int[n-1];
-        int[] dp2 = new int[n-1];
-        Arrays.fill(dp1,-1);
-        Arrays.fill(dp2,-1);
+        if (n == 1) return nums[0];
 
-        int last = f(arr22,dp2,n-2);
-        int first = f(arr11,dp1,n-2);
+        // Case 1: Rob from house 0 to n-2 (exclude last)
+        int robFirst = robLinear(nums, 0, n - 2);
         
-        return Math.max(last,first);
+        // Case 2: Rob from house 1 to n-1 (exclude first)
+        int robLast = robLinear(nums, 1, n - 1);
         
+        return Math.max(robFirst, robLast);
     }
-    private int f(int[] nums,int[] dp,int ind){
-        if(ind == 0) return nums[ind];
-        if(ind < 0) return 0;
-        if(dp[ind] != -1) return dp[ind];
 
-        int pick = nums[ind]+f(nums,dp,ind-2);
-        int notpick = f(nums,dp,ind-1);
-        dp[ind] = Math.max(pick,notpick);
-        return dp[ind];
+    private int robLinear(int[] nums, int start, int end) {
+        int prev = 0;       // dp[i-1]
+        int prev2 = 0;      // dp[i-2]
+
+        for (int i = start; i <= end; i++) {
+            int pick = nums[i] + prev2;
+            int notPick = prev;
+            int curr = Math.max(pick, notPick);
+
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return prev;
     }
 }
