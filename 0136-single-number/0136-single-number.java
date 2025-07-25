7875ff2c807905a00f254adad73687c917a63af5
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2025-07-25 16:34:15 +0530

    Time: 7 ms (28.82%), Space: 46.7 MB (10.44%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,17 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int xor = 0;
-        for(int num:nums){
-            xor = xor ^ num;
+        int n = nums.length;
+        Arrays.sort(nums);
+        int i = 0;
+        int j = 1;
+        if(n <= 1){
+            return nums[0];
         }
-        return xor;
-        
+        while(j<n && i<n && nums[i] == nums[j] ){
+            i = j+1;
+            j = i+1;
+        }
+        return nums[i];
+
     }
 }
