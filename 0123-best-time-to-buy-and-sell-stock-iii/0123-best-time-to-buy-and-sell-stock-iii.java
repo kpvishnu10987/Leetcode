36c6a43175b73a176f404ae2c27e8fbb7a23ec48
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2025-08-05 17:56:42 +0530

    Time: 66 ms (36.72%), Space: 62.6 MB (48.07%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+
+        int[][][] dp = new int[n+1][2][3];
+    
+        int profit = 0;
+        for(int i = n-1 ; i>=0 ; i--){
+            for(int canBuy = 0 ; canBuy <= 1 ; canBuy++){
+                for(int cap = 1 ; cap <= 2 ; cap++){
+                    if(canBuy == 1){
+                        int buy = -prices[i] + dp[i+1][0][cap];
+                        int skip = dp[i+1][1][cap];
+                        profit = Math.max(buy,skip);
+                    }else{
+                        int sell = prices[i] + dp[i+1][1][cap-1];
+                        int skip = dp[i+1][0][cap];
+                        profit = Math.max(sell,skip);
+                    }
+                    dp[i][canBuy][cap] = profit;
+                }
+            }
+        }
+        return dp[0][1][2];
+        
+        
+        
+    }
+}
