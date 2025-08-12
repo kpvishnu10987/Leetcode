ba64b6379092e3b4831d17a5831935fcda97a7d4
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2025-08-12 19:20:25 +0530

    Time: 13 ms (73.17%), Space: 42.6 MB (45.73%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
@@ -1,36 +1,38 @@
 class Solution {
     public int minCost(int n, int[] cuts) {
         List<Integer> li = new ArrayList<>();
-        for(int i = 0 ; i<cuts.length; i++){
+        for (int i = 0; i < cuts.length; i++) {
             li.add(cuts[i]);
         }
         li.add(0);
         li.add(n);
         Collections.sort(li);
         int[] arr = new int[li.size()];
-        for(int i = 0 ; i<li.size(); i++){
+        for (int i = 0; i < li.size(); i++) {
             arr[i] = li.get(i);
         }
         int c = cuts.length;
-        int[][] dp = new int[c+1][c+1];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
-        }
-        return helper(1,arr.length-2,arr,dp);
-    }
-    private int helper(int i ,int j ,int[] arr,int[][] dp){
-        if(i>j){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
+        int[][] dp = new int[c + 2][c + 2];
+        for (int[] row : dp) {
+            Arrays.fill(row, 0);
         }
 
-        int mini = Integer.MAX_VALUE;
-        for(int k = i ; k<=j ; k++){
-            int cost = arr[j+1]-arr[i-1]+helper(i,k-1,arr,dp)+helper(k+1,j,arr,dp);
-            mini = Math.min(mini,cost);
+        for (int i = c; i >= 1; i--) {
+            for (int j = 1; j <= c; j++) {
+                if (i > j) {
+                    continue;
+                }
+
+                int mini = Integer.MAX_VALUE;
+                for (int k = i; k <= j; k++) {
+                    int cost = arr[j + 1] - arr[i - 1] + dp[i][k - 1] + dp[k + 1][j];
+                    mini = Math.min(mini, cost);
+                }
+                dp[i][j] = mini;
+
+            }
         }
-        return dp[i][j] = mini;
+        return dp[1][c];
     }
+
 }
