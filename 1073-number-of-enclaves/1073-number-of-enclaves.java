965a9e3c20af94fc394010dfd74ffd0e0667a172
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2025-08-24 07:02:03 +0530

    Time: 8 ms (74.37%), Space: 49.8 MB (97.28%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.java
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -41,8 +41,8 @@ class Solution {
                 }
             }
         }
-        for(int i = 0; i<n ; i++){
-            for(int j = 0; j<m ; j++){
+        for(int i = 1; i<n-1 ; i++){
+            for(int j = 1; j<m-1 ; j++){
                 if(grid[i][j] == 1){
                     cnt++;
                 }
