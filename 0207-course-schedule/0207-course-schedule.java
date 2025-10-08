48e4d3f1e915c418520587fd82379608646efc7f
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2025-10-08 06:39:43 +0530

    Time: 6 ms (72.39%), Space: 46.2 MB (11.73%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,44 +1,51 @@
+class Edge {
+    int src;
+    int dest;
+    public Edge(int s, int d) {
+        this.src = s;
+        this.dest = d;
+    }
+}
+
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0 ; i<numCourses; i++){
-            adj.add(new ArrayList<>());
+        ArrayList<Edge>[] graph =  new ArrayList[numCourses];
+        
+        for(int i = 0 ;i<numCourses ; i++){
+            graph[i] = new ArrayList<>();
         }
 
-        for(int i = 0; i<prerequisites.length; i++){
+        for(int i = 0 ; i<prerequisites.length; i++){
             int a = prerequisites[i][0];
             int b = prerequisites[i][1];
-            
-            adj.get(b).add(a);
+            graph[b].add(new Edge(b,a));
         }
 
         int[] in = new int[numCourses];
-        for(int i= 0 ; i<numCourses ; i++){
-            for(int it : adj.get(i)){
-                in[it]++;
+        for(int i = 0 ; i<numCourses; i++){
+            for(Edge e : graph[i]){
+                in[e.dest]++;
             }
         }
+
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 0 ; i<numCourses ; i++){
-            if(in[i] == 0){
-                q.add(i);
-            }
+        for (int i = 0; i < numCourses; i++) {
+            if (in[i] == 0) q.add(i);
         }
+
         List<Integer> topo = new ArrayList<>();
 
         while(!q.isEmpty()){
             int node = q.poll();
             topo.add(node);
-
-            for(int it : adj.get(node)){
-                in[it]--;
-                if(in[it] == 0){
-                    q.add(it);
+            for(Edge e : graph[node]){
+                in[e.dest]--;
+                if(in[e.dest] == 0){
+                    q.add(e.dest);
                 }
             }
-
         }
-        return topo.size() == numCourses;
-        
+
+        return numCourses == topo.size();
     }
 }
