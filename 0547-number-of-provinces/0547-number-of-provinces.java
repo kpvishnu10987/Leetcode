0de52385abb3593c12f51ec5e9ca7d34fcfce773
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2025-10-21 23:31:46 +0530

    Time: 3 ms (33.26%), Space: 48.7 MB (5.4%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,23 +1,59 @@
+import java.util.*;
+
+class DisjointSet {
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+
+    public DisjointSet(int n) {
+        for (int i = 0; i < n; i++) {
+            parent.add(i);
+            size.add(1);
+        }
+    }
+
+    public int findPar(int u) {
+        if (u == parent.get(u)) return u;
+        int root = findPar(parent.get(u));
+        parent.set(u, root); 
+        return root;
+    }
+
+    public void unionBySize(int u, int v) {
+        int up_u = findPar(u);
+        int up_v = findPar(v);
+
+        if (up_u == up_v) return; 
+
+        if (size.get(up_u) < size.get(up_v)) {
+            parent.set(up_u, up_v);
+            size.set(up_v, size.get(up_v) + size.get(up_u));
+        } else {
+            parent.set(up_v, up_u);
+            size.set(up_u, size.get(up_u) + size.get(up_v));
+        }
+    }
+}
+
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        boolean[] visited = new boolean[n];
-        int result = 0;
+        DisjointSet ds = new DisjointSet(n);
 
-        for(int i = 0 ; i<n ; i++){
-            if(!visited[i]){
-                dfs(isConnected,visited,result,i);
-                result++;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1) { 
+                    ds.unionBySize(i, j);
+                }
             }
         }
-        return result;
-    }
-    private void dfs(int[][] isConnected,boolean[] visited,int result,int s){
-        visited[s] = true;
-        for(int i = 0 ; i<isConnected.length; i++){
-            if(isConnected[s][i] == 1 && !visited[i]){
-                dfs(isConnected,visited,result,i);
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (ds.findPar(i) == i) { 
+                count++;
             }
         }
+
+        return count;
     }
 }
