6f880783dd1bb927416c533542e1d8af4df880c9
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2025-10-27 16:10:04 +0530

    Time: 1 ms (79.87%), Space: 43.7 MB (17.95%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,37 +1,25 @@
 class Solution {
-    private boolean isValid(String s){
-        Stack<Character> st = new Stack<>();
-        for(int i = 0; i<s.length() ; i++){
-            char c = s.charAt(i);
-            if(c == ')'){
-                if(st.isEmpty()){
-                    return false;
-                }else{
-                    st.pop();
-                }
-            }else{
-                st.push(c);
-            }
-        }
-        if(!st.isEmpty()) return false;
-        return true;
-    }
+    
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
         String cur = "";
-        helper(n,ans,cur);
+        helper(n,ans,0,0,cur);
         return ans;
     }
-    private void helper(int n , List<String> ans,String cur){
+    private void helper(int n , List<String> ans,int open ,int close,String cur){
 
         if(cur.length() == 2*n){
-            if(isValid(cur)){
+            
                 ans.add(cur);
-            }
+            
             return;
         }
-        helper(n,ans,cur+"(");
-        helper(n,ans,cur+")");
+        if(open < n){
+            helper(n,ans,open+1,close,cur+"(");
+        }
+        if(close < open){
+            helper(n,ans,open,close+1,cur+")");
+        }
 
     }
 }
