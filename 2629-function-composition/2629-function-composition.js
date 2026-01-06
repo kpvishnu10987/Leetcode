39a9a47895f3b99a6a3242d0c19526234e37ca81
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-01-06 22:52:44 +0530

    Time: 51 ms (84.3%), Space: 55.3 MB (74.79%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -6,8 +6,7 @@ var compose = function(functions) {
     
     return function(x) {
         for(let i = functions.length-1; i>=0 ; i--){
-            let fn = functions[i];
-            x = fn(x);
+            x = functions[i](x);
         }
         return x
     }
