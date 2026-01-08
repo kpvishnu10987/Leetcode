9a4162eff1ca85417b433d3675ce0a77ecda6032
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-01-08 19:08:27 +0530

    Time: 86 ms (23.57%), Space: 46.2 MB (44.62%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,17 +1,29 @@
 class Solution {
+    int dp[];
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
+        dp = new int[n];
 
-        int[] dp = new int[n];
-        Arrays.fill(dp,1);
+        Arrays.fill(dp,-1);
 
-        for(int i = 1; i<n ;i++){
-            for(int j = 0 ; j <= i-1 ; j++){
-                if(nums[i] > nums[j]){
-                    dp[i] = Math.max(dp[i],1+dp[j]);
-                }
+        int ans = 1;
+
+        for(int i = 0 ;i<n ;i++){
+            ans = Math.max(ans,solve(i,nums));
+        }
+
+        return ans;    
+    }
+
+    private int solve(int i, int[] nums){
+        if(dp[i] != -1) return dp[i];
+
+        int best = 1;
+        for(int j = 0 ; j<i ; j++){
+            if(nums[i] > nums[j]){
+                best = Math.max(best,1+solve(j,nums));
             }
         }
-        return Arrays.stream(dp).max().getAsInt();
+        return dp[i] = best;
     }
 }
