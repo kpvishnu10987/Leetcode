ea6404614bad4c27e12755e80d3196fdd93f34b5
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-01-09 18:50:06 +0530

    Time: 2 ms (95.36%), Space: 45.7 MB (71.14%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,22 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> sub = new ArrayList<>();
-        helper(0, 0, candidates, target, ans, sub);
+        helper(0,candidates,target,ans,new ArrayList<>());
         return ans;
     }
 
-    private void helper(int i, int sum, int[] candidates, int target, List<List<Integer>> ans, List<Integer> sub) {
-        if(i == candidates.length || sum >= target){
-            if(sum == target){
-                ans.add(new ArrayList<>(sub));
+    private void helper(int i , int[] candidates,int target,List<List<Integer>> ans,List<Integer> ds){
+        if(i == candidates.length){
+            if(target == 0){
+                ans.add(new ArrayList<>(ds));
             }
             return;
         }
 
-        sub.add(candidates[i]);
-        helper(i,sum+candidates[i],candidates,target,ans,sub);
-        sub.remove(sub.size()-1);
-        helper(i+1,sum,candidates,target,ans,sub);
+        if(candidates[i] <= target){
+            ds.add(candidates[i]);
+            helper(i,candidates,target-candidates[i],ans,ds);
+            ds.remove(ds.size()-1);
+        }
+        helper(i+1,candidates,target,ans,ds);
     }
 }
