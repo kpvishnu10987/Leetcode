64b6c9955039b3e7aa5502abacf752ebaa40659a
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-01-13 19:24:10 +0530

    Time: 0 ms (100%), Space: 42.3 MB (31.88%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,21 +1,22 @@
 class Solution {
+    int[][] dp;
+    int m,n;
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+        this.m = m;
+        this.n = n;
+        dp = new int[m][n];
 
-        Arrays.fill(dp[0],1);
-        for(int i = 0 ; i<m ; i++){
-            dp[i][0] = 1;
+        for(int i = 0  ;i<m ;i++){
+            Arrays.fill(dp[i],-1);
         }
 
-        for(int i = 1 ; i<m ; i++){
-            for(int j = 1; j<n ; j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
+        return f(0,0);
+    }
+    private int f(int i,int j){
+        if(i >= m || j>=n) return 0;
+        if(i == m-1 && j == n-1) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
 
-        return dp[m-1][n-1];
-
-
-
+        return dp[i][j] = f(i+1,j)+f(i,j+1); 
     }
 }
