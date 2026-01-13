bd410a3f9b31c5db7356ae2fba13bc6cb635bcd1
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-01-13 19:41:16 +0530

    Time: 4 ms (15.84%), Space: 48.1 MB (13.14%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,18 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int max = 0;
+
         int n = nums.length;
-        for(int i = 0; i<n ;i++){
-            if(i > max) return false;
-            max = Math.max(max,i+nums[i]);
+
+        int dp[] = new int[n];
+        dp[0] = nums[0];
+
+        for(int i = 1 ; i<n ; i++){
+            if(dp[i-1] < i) return false;
+            dp[i] = Math.max(i+nums[i],dp[i-1]);
+        
         }
-        return true;
+
+        return dp[n-1] >= n-1;
+        
     }
 }
