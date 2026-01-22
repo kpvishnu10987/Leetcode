bbea6f269eb2706165b22ab95e8d544315534d0a
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-01-22 22:09:22 +0530

    Time: 6 ms (24.21%), Space: 51.4 MB (86.12%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,52 +1,45 @@
-class Edge{
-    int src;
-    int dest;
-    public Edge(int s,int d){
-        this.src = s;
-        this.dest = d;
-    }
-}
-
 class Solution {
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int[][] vis = new int[n][m];
-        Queue<Edge> q = new LinkedList();
 
-        int cnt = 0 ;
-        for(int i = 0 ; i<n ;i++){
-            for(int j = 0 ; j<m ;j++){
-                if(vis[i][j] != 1 && grid[i][j] == '1'){
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[n][m];
+        int cnt  = 0;
+        for(int i = 0 ; i<n ; i++){
+            for(int j = 0 ; j<m ; j++){
+                if(!visited[i][j] && grid[i][j] == '1'){
                     cnt++;
-                    vis[i][j] = 1;
-                    q.offer(new Edge(i,j));
-                    bfs(grid,vis,q,n,m);
+                    q.offer(new int[]{i,j});
+                    bfs(q,visited,grid);
                 }
             }
         }
         return cnt;
     }
-    public void bfs(char[][] grid,int[][] vis,Queue<Edge> q,int n ,int m){
+    private void bfs(Queue<int[]> q,boolean[][] visited,char[][] grid){
+        int n = grid.length;
+        int m = grid[0].length;
 
         int[] drow = {-1,0,+1,0};
         int[] dcol = {0,+1,0,-1};
 
         while(!q.isEmpty()){
-            Edge e = q.poll();
-            int start = e.src;
-            int end = e.dest;
-   
-            for(int i = 0; i<4 ; i++){
-                int nrow = start+drow[i];
-                int ncol = end + dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol] != 1 && grid[nrow][ncol] == '1'){
-                    vis[nrow][ncol] = 1;
-                    q.offer(new Edge(nrow,ncol));
+            int[] p = q.poll();
+            for(int i = 0 ; i<4 ;i++){
+                int nr = p[0] + drow[i];
+            int nc = p[1] + dcol[i];
+
+            if(nr>=0 && nr<n && nc >=0 && nc <m && !visited[nr][nc]){
+                visited[nr][nc] = true;
+                if(grid[nr][nc] == '1'){
+                    q.offer(new int[]{nr,nc});
                 }
             }
-
+            }
         }
+
+
 
     }
 }
