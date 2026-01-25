5ca6cfe7b6dca5627322f4990067dce420b3376b
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-01-25 12:24:49 +0530

    Time: 4 ms (79.72%), Space: 44.5 MB (72.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,19 +2,19 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         int n = s.length();
         int m = t.length();
-        if(n!=m) return false;
-        int[] arr = new int[126];
-        for(int i = 0 ; i<n ;i++){
-            int idx = s.charAt(i)-'a';
-            arr[idx]++;
-        }
-        for(int i = 0 ;i<m ;i++){
-            int idx = t.charAt(i)-'a';
-            if(arr[idx] <= 0) return false;
-            arr[idx]--;
+        if(n != m) return false;
+
+        int[] sarr = new int[26];
+        int[] tarr = new int[26];
+        for(int i = 0 ; i<n ; i++){
+            sarr[s.charAt(i)-'a']++;
+            tarr[t.charAt(i)-'a']++;
         }
 
-        return true;
+        for(int i = 0 ; i<26 ; i++){
+            if(sarr[i] != tarr[i]) return false;
+        }
         
+        return true;
     }
 }
