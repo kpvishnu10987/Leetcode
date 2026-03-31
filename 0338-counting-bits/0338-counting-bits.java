a2cef3ba1f28c728adbd890bd17faf2b3e86e664
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-03-31 23:54:26 +0530

    Time: 2 ms (96.55%), Space: 48.5 MB (79.97%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,17 +1,10 @@
 class Solution {
     public int[] countBits(int n) {
         int[] arr = new int[n+1];
-        for(int i= 0 ; i<=n ;i++){
-            arr[i] = hammingWeights(i);
+        for(int i = 1; i<= n ; i++){
+            arr[i] = arr[i & (i-1)]+1;
         }
         return arr;
-    }
-    private int hammingWeights(int i){
-        int count  =0;
-        while(i != 0){
-            i = i & i-1;
-            count++;
-        }
-        return count;
+        
     }
 }
