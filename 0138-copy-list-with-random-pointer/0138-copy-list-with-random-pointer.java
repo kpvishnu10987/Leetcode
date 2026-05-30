5f980d7b018d91df13502a494281e8a4e4a8f753
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-05-30 21:25:51 +0530

    Time: 0 ms (100%), Space: 46.5 MB (69.27%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    Map<Node,Node> map = new HashMap<>();
+    public Node copyRandomList(Node head) {
+        if(head == null) return head;
+
+        if(map.containsKey(head)){
+            return map.get(head);
+        }
+
+        Node clone = new Node(head.val);
+        map.put(head,clone);
+
+        clone.next = copyRandomList(head.next);
+        clone.random = copyRandomList(head.random);
+
+        return clone;
+    }
+}
