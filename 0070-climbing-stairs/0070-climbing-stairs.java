55e515177048e29e8cc387eac68da8a7557f3e8b
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-06-04 22:47:17 +0530

    Time: 0 ms (100%), Space: 41.8 MB (92.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,14 @@
 class Solution {
-    int[] dp;
     public int climbStairs(int n) {
-        dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(n);   
-    }
+        int[] dp = new int[n+1];
+        if(n <= 2) return n;
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
 
-    private int solve(int n){
-        if (n <=2) return n;
-        if(dp[n] != -1) return dp[n];
-
-        dp[n] = solve(n-1)+solve(n-2);
+        for(int i = 3 ; i<= n;  i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
         return dp[n];
     }
 }
