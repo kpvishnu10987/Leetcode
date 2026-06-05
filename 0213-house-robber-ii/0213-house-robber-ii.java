1d2d28b5daef04f9557837556a1b18878cdef61b
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-06-05 12:27:09 +0530

    Time: 0 ms (100%), Space: 42.7 MB (76.07%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,30 +1,21 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 1) return nums[0];
 
-        // Case 1: Rob from house 0 to n-2 (exclude last)
-        int robFirst = robLinear(nums, 0, n - 2);
-        
-        // Case 2: Rob from house 1 to n-1 (exclude first)
-        int robLast = robLinear(nums, 1, n - 1);
-        
-        return Math.max(robFirst, robLast);
+        if(n == 1) return nums[0];
+
+        return Math.max(dfs(nums,0,n-2),dfs(nums,1,n-1));
     }
 
-    private int robLinear(int[] nums, int start, int end) {
-        int prev = 0;       // dp[i-1]
-        int prev2 = 0;      // dp[i-2]
-
-        for (int i = start; i <= end; i++) {
-            int pick = nums[i] + prev2;
-            int notPick = prev;
-            int curr = Math.max(pick, notPick);
-
-            prev2 = prev;
-            prev = curr;
+    private int dfs(int[] nums,int start,int end){
+        int prev1 = 0;
+        int prev2 = 0;
+        for(int i = start ; i<=end ; i++){
+            int cur = Math.max(prev1,nums[i]+prev2);
+            prev2 = prev1;
+            prev1 = cur;
         }
 
-        return prev;
+        return prev1;
     }
 }
