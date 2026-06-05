45e593ad531e7834409a7cb9732f21e60fd931b3
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-06-05 22:17:56 +0530

    Time: 1 ms (83%), Space: 43.1 MB (48.29%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -2,21 +2,26 @@ class Solution {
     public int numDecodings(String s) {
         int n = s.length();
         int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = (s.charAt(0) != '0') ? 1 :0;
-        
-        for(int i = 2  ; i<=n ;i++){
-            int single = (s.charAt(i-1) != '0')?dp[i-1]:0;
-            int doubl = 0;
-            int val = (s.charAt(i-2)-'0')*10 + (s.charAt(i-1)-'0');
-            if (val >= 10 && val <= 26){
-                doubl = dp[i-2];
+        dp[n] = 1;
+
+        for(int i = n-1 ; i>=0 ; i--){
+            if(s.charAt(i) == '0'){
+                dp[i] = 0;
+                continue;
             }
 
-            dp[i] = single+doubl;
+            dp[i] = dp[i+1];
+
+            if(i+1 < n){
+                int num = (s.charAt(i)-'0')*10 + s.charAt(i+1)-'0';
+
+                if(num>9 && num<27){
+                    dp[i] += dp[i+2];
+                }
+            }
+
         }
 
-        return dp[n]>0?dp[n]:0;
-        
+        return dp[0];
     }
 }
