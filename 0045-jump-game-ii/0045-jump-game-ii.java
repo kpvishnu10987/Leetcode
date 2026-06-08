24eab66f3c6ac0051e3da92ace85f6a50904279d
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-06-08 23:06:10 +0530

    Time: 1 ms (99.69%), Space: 47.2 MB (55.23%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,15 +1,14 @@
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        int farthest = 0;
-        int end = 0;
+        int cur = 0;
+        int far = 0;
         int jumps = 0;
-
-        for (int i = 0; i < n - 1; i++) {
-            farthest = Math.max(farthest, i + nums[i]);
-            if (i == end) {
+        for(int i = 0; i<n-1 ; i++){
+            far = Math.max(far,i+nums[i]);
+            if(i == cur){
                 jumps++;
-                end = farthest;
+                cur = far;
             }
         }
 
