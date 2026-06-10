6e7e7b05b8b65f8f25cd7c3a503c4069dc3f70e0
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-06-10 19:31:45 +0530

    Time: 1 ms (97.94%), Space: 46.9 MB (87.3%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,24 +1,46 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int n = intervals.length;
+        List<int[]> ans = new ArrayList<>();
+        if(n == 0){
+            int[][] me = new int[1][2];
+            me[0][0] = newInterval[0];
+            me[0][1] = newInterval[1];
+            return me;
+        }
 
+        int i = 0;
+        int start = newInterval[0];
+        int end = newInterval[1];
+        boolean added = false;
 
-        List<int[]> result = new ArrayList<>();
-
-        for(int[] interval : intervals){
-            if(interval[1] < newInterval[0]){
-                result.add(interval);
-            }else if(interval[0] > newInterval[1]){
-                result.add(newInterval);
-                newInterval = interval;
+        while(i < n){
+            int[] interval = intervals[i];
+            if(start > interval[1]){
+                ans.add(interval);
+                i++;
+                continue;
             }else{
-                newInterval[0] = Math.min(newInterval[0],interval[0]);
-                newInterval[1] = Math.max(newInterval[1],interval[1]);
+                added = true;
+                start = Math.min(start,interval[0]);
+                while(i < n && end >= intervals[i][0]){
+                    end = Math.max(end,intervals[i][1]);
+                    i++;
+                }
+                ans.add(new int[]{start,end});
+                break;
                 
             }
         }
-        result.add(newInterval);
-        
-        return result.toArray(new int[result.size()][]);
+        if(!added){
+            ans.add(newInterval);
+        }
+
+        while(i < n){
+            ans.add(intervals[i]);
+            i++;
+        }
+
+        return ans.toArray(new int[0][]);
     }
 }
