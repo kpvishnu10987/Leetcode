9b6f1aa72e8cf6435426bc42661123407520acb8
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-05 17:35:37 +0530

    Time: 11 ms (50%), Space: 48 MB (100%) - LeetHub

---
+++ 3980-minimum-operations-to-transform-binary-string/3980-minimum-operations-to-transform-binary-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minOperations(String s1, String s2) {
+        if (s1.equals("1") && s2.equals("0")) {
+            return -1;
+        }
+        char[] s = s1.toCharArray();
+        int res = 0, n = s1.length();
+        for (int i = 0; i < n; i++) {
+            if (s[i] == s2.charAt(i)) continue;
+            res++;
+            if (s[i] == '1') {
+                if (i == n - 1) {
+                    res++;
+                } else {
+                    res += s[i + 1] == '0' ? 1 : 0;
+                    s[i + 1] = '0';
+                }
+            }
+        }
+        return res;
+    }
+}
