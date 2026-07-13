a686a7278c71924593d7c06323ec7e04362d6bbc
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-13 14:22:25 +0530

    Time: 3 ms (98.37%), Space: 47.3 MB (17.98%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,27 +1,42 @@
 class Solution {
+    String w1;
+    String w2;
+    int n;
+    int m;
+    int[][] dp;
     public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-
-        int[][] dp = new int[n+1][m+1];
-
-        for(int i = 0 ; i<=n ; i++){
-            dp[i][0] = i; 
-        }
-        for(int j = 0 ; j<=m ; j++){
-            dp[0][j] = j;
+        w1 = word1;
+        w2 = word2;
+        n = w1.length();
+        m = w2.length();
+        dp = new int[n][m];
+        for(int[] r : dp){
+            Arrays.fill(r,-1);
         }
 
-        for(int i = 1; i<= n ; i++){
-            for(int j = 1 ; j<=m ; j++){
-                if(word1.charAt(i-1) == word2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
-                }else{
-                    dp[i][j] = 1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
-                }
+        return dfs(0,0);
+    }
+
+    private int dfs(int i,int j){
+        if(j == m){
+            if(i == n){
+                return 0;
+            }else{
+                return n-i;
             }
         }
-        return dp[n][m];
- 
+        if(i == n) return m-j;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+
+
+        if(w1.charAt(i) == w2.charAt(j)) return dp[i][j] = dfs(i+1,j+1);
+
+        int ins = dfs(i,j+1);
+        int del = dfs(i+1,j);
+        int rep = dfs(i+1,j+1);
+
+        return dp[i][j] =  1 + Math.min(ins,Math.min(del,rep));
     }
 }
