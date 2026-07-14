46e22aedba050e7a0eac6405f40c377f8f145bff
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-14 19:04:35 +0530

    Time: 22 ms (27.79%), Space: 48.2 MB (6.78%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,32 +1,48 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s == null || t == null || s.length() < t.length()) return "";
+        int n = s.length();
+        int m = t.length();
 
-        int[] targetFreq = new int[128];
-        for (char c : t.toCharArray()) targetFreq[c]++;
+        Map<Character, Integer> smap = new HashMap<>();
+        Map<Character, Integer> tmap = new HashMap<>();
+        int count = 0;
 
-        int left = 0, right = 0, start = 0, minLen = Integer.MAX_VALUE;
-        int count = t.length();
-
-        while (right < s.length()) {
-            char rChar = s.charAt(right);
-            if (targetFreq[rChar] > 0) count--;
-            targetFreq[rChar]--;
-            right++;
-
-            while (count == 0) {
-                if (right - left < minLen) {
-                    minLen = right - left;
-                    start = left;
-                }
-
-                char lChar = s.charAt(left);
-                targetFreq[lChar]++;
-                if (targetFreq[lChar] > 0) count++;
-                left++;
-            }
+        for (char c : t.toCharArray()) {
+            tmap.put(c, tmap.getOrDefault(c, 0) + 1);
+            count++;
         }
 
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+        int l = 0;
+        int start = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int r = 0; r < n; r++) {
+            char rc = s.charAt(r);
+
+            smap.put(rc, smap.getOrDefault(rc, 0) + 1);
+            if(tmap.containsKey(rc) && smap.get(rc) <= tmap.get(rc)){
+                count--;
+            }
+
+            while (count == 0) {
+                char lc = s.charAt(l);
+                if(min > r-l+1){
+                    min = r-l+1;
+                    start = l;
+                }
+                
+
+                smap.put(lc, smap.get(lc) - 1);
+                if(tmap.containsKey(lc) && smap.get(lc) < tmap.get(lc)){
+                    count++;
+                }
+                if (smap.get(lc) <= 0)
+                    smap.remove(lc);
+                l++;
+            }
+        }
+        return min == Integer.MAX_VALUE ? "" : s.substring(start,start+min);
     }
+
+
 }
