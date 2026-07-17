f2c3353fa1a1271459e3fab08382912af706b43f
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-17 14:29:32 +0530

    Time: 3 ms (26.45%), Space: 49.5 MB (11.67%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,59 +1,43 @@
-import java.util.*;
-
-class DisjointSet {
-    List<Integer> parent = new ArrayList<>();
-    List<Integer> size = new ArrayList<>();
-
-    public DisjointSet(int n) {
-        for (int i = 0; i < n; i++) {
-            parent.add(i);
-            size.add(1);
-        }
-    }
-
-    public int findPar(int u) {
-        if (u == parent.get(u)) return u;
-        int root = findPar(parent.get(u));
-        parent.set(u, root); 
-        return root;
-    }
-
-    public void unionBySize(int u, int v) {
-        int up_u = findPar(u);
-        int up_v = findPar(v);
-
-        if (up_u == up_v) return; 
-
-        if (size.get(up_u) < size.get(up_v)) {
-            parent.set(up_u, up_v);
-            size.set(up_v, size.get(up_v) + size.get(up_u));
-        } else {
-            parent.set(up_v, up_u);
-            size.set(up_u, size.get(up_u) + size.get(up_v));
-        }
-    }
-}
-
 class Solution {
+    List<List<Integer>> graph;
+    boolean vis[];
     public int findCircleNum(int[][] isConnected) {
+        graph = new ArrayList<>();
         int n = isConnected.length;
-        DisjointSet ds = new DisjointSet(n);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1) { 
-                    ds.unionBySize(i, j);
+        for(int i = 0 ; i<n ; i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int i = 0 ; i<n ; i++){
+            for(int j = 0 ; j<n ; j++){
+                if(isConnected[i][j] == 1){
+                    graph.get(i).add(j);
                 }
             }
         }
 
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (ds.findPar(i) == i) { 
-                count++;
+        vis = new boolean[n];
+        int comp = 0;
+
+
+
+        for(int i = 0 ; i<n ;i++){
+            if(!vis[i]){
+                comp++;
+                dfs(i);
             }
         }
 
-        return count;
+        return comp;
+    }
+
+    private void dfs(int i){
+        vis[i] = true;
+
+        for(int nei : graph.get(i)){
+            if(!vis[nei]){
+                dfs(nei);
+
+            }
+        }
     }
 }
