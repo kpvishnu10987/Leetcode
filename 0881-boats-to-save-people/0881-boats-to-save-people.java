6d44065859941e9ae11b33cab906456b17f918b1
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-21 14:00:30 +0530

    Time: 21 ms (24.74%), Space: 56.6 MB (38.13%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int n = people.length;
+
+        int l = 0;
+        int r = n-1;
+
+        int boats = 0;
+
+        while(l<r){
+            if(people[l]+people[r] <= limit){
+                l++;
+                r--;
+            }else{
+                r--;
+            }
+            boats++;
+        }
+        if(l == r) boats++;
+
+        return boats;
+
+    }
+}
