0962bee0213eabf1912d1cb29fa3a37c0292763c
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-23 22:02:32 +0530

    Time: 127 ms (14.1%), Space: 82.2 MB (16.67%) - LeetHub

--- 2931-maximum-spending-after-buying-items/2931-maximum-spending-after-buying-items.java
+++ 2931-maximum-spending-after-buying-items/2931-maximum-spending-after-buying-items.java
@@ -1,40 +1,22 @@
-class Pair{
-    int val;
-    int r;
-    int c;
-    Pair(int val,int r,int c){
-        this.val = val;
-        this.r = r;
-        this.c = c;
-    }
-}
+
 class Solution {
     public long maxSpending(int[][] values) {
         int n = values.length;
         int m = values[0].length;
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.val-b.val);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for(int i = 0 ; i<n ; i++){
-            pq.offer(new Pair(values[i][m-1],i,m-1));
+            for(int j = 0 ; j<m ; j++){
+                pq.offer(values[i][j]);
+            }
         }
         long d = 1;
         long ans = 0;
-
         while(!pq.isEmpty()){
-            Pair node = pq.poll();
-            int v = node.val;
-            int i = node.r;
-            int j = node.c;
-
-            ans += d * v;
+            ans += d * pq.poll();
             d++;
-
-            if(j-1 >= 0){
-                pq.offer(new Pair(values[i][j-1],i,j-1));
-            }
         }
-
         return ans;
     }
 }
