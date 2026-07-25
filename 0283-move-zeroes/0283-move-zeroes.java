1ccfd014bde186e11147af49e2a9144cd8b6c5ac
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-25 17:40:18 +0530

    Time: 1 ms (99.87%), Space: 47.4 MB (96.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,20 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j = 0;
-        for(int i=0 ; i<nums.length; i++){
-            if(nums[i] != 0){
-                nums[j] = nums[i];
-                j++;
+        int n = nums.length;
+
+        int l = 0;
+
+        for(int r = 0 ; r<n ; r++){
+            if(nums[r] != 0){
+                nums[l++] = nums[r];
             }
         }
-        while(j<nums.length){
-            nums[j] = 0;
-            j++;
-        }
+
         
+
+        for(int i = l ;i<n ; i++){
+            nums[i] = 0;
+        }
+
     }
 }
