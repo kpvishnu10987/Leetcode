55d8228c46b3e4b3e0c1bc2dbbcdec7888046abf
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-25 14:07:39 +0530

    Time: 1 ms (92.72%), Space: 45.3 MB (62.99%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,11 +1,13 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ones=0,twos=0;
-        for(int i=0;i<nums.length;i++){
-            ones = (ones ^ nums[i])& ~twos;
-            twos = (twos ^ nums[i])& ~ones;
+        int ones = 0;
+        int twos = 0;
+
+        for(int num : nums){
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
         }
-        return ones;
-        
+
+        return ones | twos;
     }
 }
