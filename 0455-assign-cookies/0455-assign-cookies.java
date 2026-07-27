8c41170b79fb65bc8043ef16b4d135bcf102c1e2
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-27 23:12:15 +0530

    Time: 14 ms (92.15%), Space: 51.5 MB (55.82%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,25 +1,20 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        int n = g.length;
-        int m = s.length;
-        int child = 0;
-
         Arrays.sort(g);
         Arrays.sort(s);
+
 
         int i = 0;
         int j = 0;
 
-        while(i < n && j < m){
+        while(i < g.length && j < s.length){
             if(s[j] >= g[i]){
                 i++;
                 j++;
-                child++;
             }else{
                 j++;
             }
         }
-
-        return child;
+        return i;
     }
 }
