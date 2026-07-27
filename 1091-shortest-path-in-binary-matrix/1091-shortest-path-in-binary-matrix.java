d0f557d420342d639011adea4514a88dba44fe72
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-27 13:05:02 +0530

    Time: 17 ms (42.61%), Space: 48 MB (20.24%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        if(grid[0][0] == 1 || grid[n-1][m-1] ==1) return -1;
+
+        boolean vis[][] = new boolean[n][m];
+
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] { 0, 0 });
+        int path = 0;
+        int[] drow = { -1, -1, -1, 0, 0, 1, 1, 1 };
+        int[] dcol = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            path++;
+
+            for (int k = 0; k < size; k++) {
+                int[] node = q.poll();
+                if (node[0] == n - 1 && node[1] == m - 1)
+                    return path;
+                for (int i = 0; i < 8; i++) {
+                    int nr = node[0] + drow[i];
+                    int nc = node[1] + dcol[i];
+                    if (nr >= 0 && nr <= n - 1 && nc >= 0 && nc <= m - 1 && vis[nr][nc] == false && grid[nr][nc] == 0) {
+                        vis[nr][nc] = true;
+                        q.offer(new int[] { nr, nc });
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
