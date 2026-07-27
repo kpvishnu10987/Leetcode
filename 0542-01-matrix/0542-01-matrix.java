f4f200c0cc21cd39b0751f3ad200cb52498f7174
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-27 13:24:58 +0530

    Time: 176 ms (5.46%), Space: 49.3 MB (36.47%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,37 +1,38 @@
 class Solution {
-    public int[][] updateMatrix(int[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
-        int[][] ans = new int[n][m];
-        for(int[] row : ans){
-            Arrays.fill(row,-1);
-        }
-
+    public int[][] updateMatrix(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean[][] vis = new boolean[n][m];
         Queue<int[]> q = new LinkedList<>();
-        for(int i = 0 ; i<n ; i++){
-            for(int j = 0 ; j<m ; j++){
-                if(mat[i][j] == 0){
-                    ans[i][j] = 0;
-                    q.offer(new int[]{i,j});
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 0) {
+                    vis[i][j] = true;
+                    q.offer(new int[] { i, j });
                 }
             }
         }
+        int[] drow = { -1, 0, 1, 0 };
+        int[] dcol = { 0, 1, 0, -1 };
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int k = 0; k < size; k++) {
+                int[] node = q.poll();
+                int r = node[0];
+                int c = node[1];
+                for (int i = 0; i < 4; i++) {
+                    int nr = node[0] + drow[i];
+                    int nc = node[1] + dcol[i];
+                    if (nr >= 0 && nr <= n - 1 && nc >= 0 && nc <= m - 1 && grid[nr][nc] == 1 && !vis[nr][nc]) {
+                        vis[nr][nc]=true;
 
-        int[] drow = {-1,0,+1,0};
-        int[] dcol = {0,+1,0,-1};
-        
-        while(!q.isEmpty()){
-            int[] point = q.poll();
-            for(int i = 0 ; i<4 ; i++){
-                int nr = point[0] + drow[i];
-                int nc = point[1] + dcol[i];
-                if(nr >= 0 && nc >= 0 && nr < n && nc < m && ans[nr][nc] == -1){
-                    ans[nr][nc] = ans[point[0]][point[1]] +1;
-                    q.offer(new int[]{nr,nc});
+                        grid[nr][nc] = 1 + grid[r][c];
+                        
+                        q.offer(new int[] { nr, nc });
+                    }
                 }
             }
         }
-
-        return ans;
+        return grid;
     }
 }
