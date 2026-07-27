bb0eceb2f5ace2750868961cef94f6c14779fa17
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-28 00:07:45 +0530

    Time: 1 ms (99.73%), Space: 47.3 MB (57.67%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -4,8 +4,10 @@ class Solution {
         int cur = 0;
         int far = 0;
         int jumps = 0;
-        for(int i = 0; i<n-1 ; i++){
-            far = Math.max(far,i+nums[i]);
+
+        for(int i = 0 ; i<n-1; i++){
+            far = Math.max(far,i + nums[i]);
+
             if(i == cur){
                 jumps++;
                 cur = far;
