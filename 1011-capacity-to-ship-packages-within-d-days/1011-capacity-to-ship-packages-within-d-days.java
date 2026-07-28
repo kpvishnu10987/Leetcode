72f3ddc1828d7946c3cbc80f1a6c0c4d0e660fa4
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-28 20:01:28 +0530

    Time: 11 ms (67.18%), Space: 50 MB (76.85%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,40 +1,37 @@
 class Solution {
-    public int shipWithinDays(int[] weights, int days) {
-        int low = 0, high = 0;
+    public int shipWithinDays(int[] nums, int days) {
+        int n =  nums.length;
 
-        for (int w : weights) {
-            low = Math.max(low, w); // min capacity
-            high += w;              // max capacity
+        int l = 0;
+        int r = 0;
+        for(int i = 0 ; i<n ; i++){
+            r += nums[i];
+            l = Math.max(l,nums[i]);
         }
 
-        int ans = high;
+        while(l < r){
+            int mid = l + (r-l)/2;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (canShip(weights, days, mid)) {
-                ans = mid;
-                high = mid - 1; // try smaller
-            } else {
-                low = mid + 1;  // increase capacity
+            if(can(mid,nums,days)){
+                r = mid;
+            }else{
+                l = mid+1;
             }
         }
-
-        return ans;
+        return l;
     }
-
-    private boolean canShip(int[] weights, int days, int cap) {
-        int current = 0;
-        int d = 1;
-
-        for (int w : weights) {
-            if (current + w > cap) {
-                d++;
-                current = 0;
+    private boolean can(int k,int[] arr,int days){
+        int n = arr.length;
+        int day = 1;
+        int pre = 0;
+        for(int i = 0 ; i<n ;i++){
+            if(pre+arr[i] > k){
+                pre = arr[i];
+                day++;
+            }else{
+                pre += arr[i];
             }
-            current += w;
         }
-
-        return d <= days;
+        return day <= days;
     }
 }
