8ab041c61188e35c5e974d6d51cb9b098c24b222
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-29 00:04:56 +0530

    Time: 10 ms (4.4%), Space: 47.7 MB (69.4%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,27 +1,28 @@
 class Solution {
-    public int trap(int[] height) {
-        int n = height.length;
-        int l = 0;
-        int r = n-1;
-        int lmax = 0,rmax = 0,total = 0;
-        while(l<r){
-            if(height[l] <= height[r]){
-                if(lmax > height[l]){
-                    total += lmax-height[l];
-                }else{
-                    lmax = height[l];
-                }
-                l++;
-            }else{
-                if(rmax > height[r]){
-                    total += rmax - height[r];
-                }else{
-                    rmax = height[r];
-                }
-                r--;
-            }
+    public int trap(int[] arr) {
+        int n = arr.length;
+
+        int[] nge = new int[n];
+        nge[n-1] = 0;
+
+        int max = arr[n-1];
+        for(int i = n-2 ; i >= 0 ; i--){
+            max = Math.max(max,arr[i]);
+            nge[i] = max;
         }
-        return total;
-        
+
+        int pre = 0;
+        int water = 0;
+
+        for(int i  = 0 ; i<n ; i++){
+            int h = Math.min(pre,nge[i]);
+            int cur = Math.max(0,h-arr[i]);
+            water += cur;
+            pre = Math.max(pre,arr[i]);
+        }
+
+        System.out.print(Arrays.toString(nge));
+
+        return water;
     }
 }
