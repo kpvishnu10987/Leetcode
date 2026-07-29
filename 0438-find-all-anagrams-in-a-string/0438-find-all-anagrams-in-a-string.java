d133a5884c5ed40196a346c50c6f1f56771e3d0a
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-29 10:50:04 +0530

    Time: 10 ms (80.69%), Space: 46.9 MB (59.09%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -5,27 +5,28 @@ class Solution {
         int m = p.length();
         if(n < m) return ans;
 
-        for(int i = 0 ; i<n-m+1 ;i++){
-            if(isAnagrams(s.substring(i,i+m),p)){
-                ans.add(i);
+        int[] need = new int[26];
+        for(int i = 0 ; i< m; i++){
+            need[p.charAt(i)-'a']++;
+        }
+        int l =0;
+        int[] matched = new int[26];
+
+        for(int r = 0 ; r<n ; r++){
+            matched[s.charAt(r)-'a']++;
+
+            if(r-l+1 > m){
+                matched[s.charAt(l)-'a']--;
+                l++;
+            }
+
+            if(r-l+1 == m){
+                if(Arrays.equals(need,matched)){
+                    ans.add(l);
+                }
             }
         }
 
         return ans;
-    }
-    private boolean isAnagrams(String s,String p){
-        int n = s.length();
-        int m = p.length();
-        int arr[] = new int[26];
-
-        for(int i = 0 ; i<n ;i++){
-            arr[s.charAt(i)-'a']++;
-            arr[p.charAt(i)-'a']--;
-        }
-
-        for(int i = 0 ;i<26 ; i++){
-            if(arr[i] != 0) return false;
-        }
-        return true;
     }
 }
