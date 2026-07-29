874b787a1387b781b3327009640344e45986a802
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-29 09:54:19 +0530

    Time: 8 ms (35.09%), Space: 49.9 MB (33.16%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,52 +1,23 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        int n = strs.length;
         List<List<String>> ans = new ArrayList<>();
-        // if(n == 1) {
-        //     ans.add(new ArrayList<>(strs[0]));
-        //     return ans;
-        // }
-        // if(n == 0){
-        //     ans.add(new ArrayList<>());
-        // } 
+        Map<String,List<String>> map = new HashMap<>();
 
-        int[] vis = new int[n];
+        for(String s : strs){
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
 
-        for(int i = 0 ; i<n ;i++){
-            List<String> sub = new ArrayList<>();
-            if(vis[i] == 1) continue;
-            sub.add(strs[i]);
-            vis[i] = 1;
-            for(int j = i+1 ; j<n ; j++){
-                if(vis[j] != 1){
-                    if(isAnagram(strs[i],strs[j])){
-                        sub.add(strs[j]);
-                        vis[j] = 1;
-                    }
-                }
-            }
-            ans.add(new ArrayList<>(sub));
+            if(!map.containsKey(key)) map.put(key,new ArrayList<>());
+            List<String> sub = map.get(key);
+            sub.add(s);
+            map.put(key,sub);
+        }
 
-        } 
+        for(String key : map.keySet()){
+            ans.add(map.get(key));
+        }
 
         return ans;
-        
-    }
-
-    boolean isAnagram(String s,String t){
-        if(s.length() != t.length()) return false;
-
-        int[] ss = new int[26];
-        int[] tt = new int[26];
-        for(int i = 0 ; i<s.length() ; i++){
-            ss[s.charAt(i)-'a']++;
-            tt[t.charAt(i)-'a']++;
-        }
-
-        for(int i = 0 ;i < 26 ; i++){
-            if(ss[i] != tt[i]) return false;
-        }
-
-        return true;
     }
 }
