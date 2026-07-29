d585c7eee167ad4eda43d498be7c02b598cc2828
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-29 19:32:04 +0530

    Time: 108 ms (45.28%), Space: 44.6 MB (18.04%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,32 +1,38 @@
 class Solution {
+    int[][] dp;
     public int maxCoins(int[] nums) {
         int n = nums.length;
-        
-        // Step 1: Create new array with 1 padding on both sides
-        int[] balloons = new int[n + 2];
-        balloons[0] = 1;
-        balloons[n + 1] = 1;
-        for (int i = 0; i < n; i++) {
-            balloons[i + 1] = nums[i];
+        int[] arr = new int[n+2];
+        arr[0] = 1;
+        arr[n+1] = 1;
+
+        dp = new int[arr.length][arr.length];
+
+        for(int[] r : dp){
+            Arrays.fill(r,-1);
         }
-        
-        // Step 2: DP array
-        int[][] dp = new int[n + 2][n + 2];
-        
-        // Step 3: Iterate over lengths of subarrays
-        for (int len = 2; len < n + 2; len++) { // len is the distance between l and r
-            for (int left = 0; left < n + 2 - len; left++) {
-                int right = left + len;
-                // Step 4: Try every balloon as the last burst in (left, right)
-                for (int k = left + 1; k < right; k++) {
-                    int coins = balloons[left] * balloons[k] * balloons[right];
-                    coins += dp[left][k] + dp[k][right];
-                    dp[left][right] = Math.max(dp[left][right], coins);
-                }
-            }
+
+        for(int i = 0 ; i<n ;i++){
+            arr[i+1] = nums[i];
         }
-        
-        // Step 5: Result for bursting all balloons between 0 and n+1
-        return dp[0][n + 1];
+
+        return f(arr,1,n);
+
+
+    }
+    private int f(int[] arr,int i,int j){
+        if(i >j) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int cost = 0;
+        int max = Integer.MIN_VALUE;
+
+        for(int k = i ; k<=j ; k++){
+            cost = arr[i-1] * arr[k] * arr[j+1] + f(arr,i,k-1) + f(arr,k+1,j);
+            max = Math.max(max,cost);
+        } 
+
+        return dp[i][j] =max;
     }
 }
