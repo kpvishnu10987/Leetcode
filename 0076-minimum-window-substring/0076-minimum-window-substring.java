75d202fd2d96750e7c67f3c7b3b4aa57c6864960
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-29 10:28:04 +0530

    Time: 23 ms (24.53%), Space: 47.9 MB (13.91%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -3,46 +3,45 @@ class Solution {
         int n = s.length();
         int m = t.length();
 
-        Map<Character, Integer> smap = new HashMap<>();
-        Map<Character, Integer> tmap = new HashMap<>();
-        int count = 0;
+        if(n < m) return new String();
+        
 
-        for (char c : t.toCharArray()) {
-            tmap.put(c, tmap.getOrDefault(c, 0) + 1);
-            count++;
+        int tcount = 0;
+        Map<Character,Integer> smap = new HashMap<>();
+        Map<Character,Integer> tmap = new HashMap<>();
+
+        for(char c : t.toCharArray()){
+            tmap.put(c,tmap.getOrDefault(c,0)+1);
+            tcount++;
         }
-
-        int l = 0;
-        int start = 0;
+        int l= 0;
         int min = Integer.MAX_VALUE;
+        int start = 0;
 
-        for (int r = 0; r < n; r++) {
+        for(int r = 0 ; r<n ; r++){
             char rc = s.charAt(r);
+            smap.put(rc,smap.getOrDefault(rc,0)+1);
+            if(tmap.containsKey(rc) && smap.get(rc) <= tmap.get(rc)) tcount--;
 
-            smap.put(rc, smap.getOrDefault(rc, 0) + 1);
-            if(tmap.containsKey(rc) && smap.get(rc) <= tmap.get(rc)){
-                count--;
-            }
-
-            while (count == 0) {
+            while(tcount == 0){
                 char lc = s.charAt(l);
                 if(min > r-l+1){
-                    min = r-l+1;
                     start = l;
+                    min = r-l+1;
                 }
-                
-
-                smap.put(lc, smap.get(lc) - 1);
-                if(tmap.containsKey(lc) && smap.get(lc) < tmap.get(lc)){
-                    count++;
-                }
-                if (smap.get(lc) <= 0)
+                int count = smap.get(lc);
+                if(count == 1){
                     smap.remove(lc);
+                }else{
+                    smap.put(lc,smap.get(lc)-1);
+                }
+                 if(tmap.containsKey(lc) && tmap.get(lc) > smap.getOrDefault(lc,0)){
+                        tcount++;
+                    }
                 l++;
             }
+
         }
-        return min == Integer.MAX_VALUE ? "" : s.substring(start,start+min);
+            return min == Integer.MAX_VALUE ? "" :s.substring(start,start+min);
     }
-
-
 }
