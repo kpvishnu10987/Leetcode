2807a0a714c11126632d46a1259f7140c4269f98
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-29 09:58:02 +0530

    Time: 3 ms (93.64%), Space: 46.7 MB (9.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,19 +2,15 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         int n = s.length();
         int m = t.length();
-        if(n != m) return false;
 
-        int[] sarr = new int[26];
-        int[] tarr = new int[26];
-        for(int i = 0 ; i<n ; i++){
-            sarr[s.charAt(i)-'a']++;
-            tarr[t.charAt(i)-'a']++;
-        }
+        char[] sarr = s.toCharArray();
+        char[] tarr = t.toCharArray();
+        Arrays.sort(sarr);
+        Arrays.sort(tarr);
 
-        for(int i = 0 ; i<26 ; i++){
-            if(sarr[i] != tarr[i]) return false;
-        }
-        
-        return true;
+        String news = new String(sarr);
+        String newt = new String(tarr);
+
+        return news.equals(newt);
     }
 }
