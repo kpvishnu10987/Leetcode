57ade0908a12967ed254bb858aca9e7e12321540
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-30 15:48:09 +0530

    Time: 1 ms (59.37%), Space: 47.5 MB (91.91%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,27 +1,23 @@
 class Solution {
-    public int trap(int[] arr) {
-        int n = arr.length;
+    public int trap(int[] nums) {
+        int n = nums.length;
 
-        int[] nge = new int[n];
-        nge[n-1] = 0;
-
-        int max = arr[n-1];
-        for(int i = n-2 ; i >= 0 ; i--){
-            max = Math.max(max,arr[i]);
-            nge[i] = max;
+        int[] rmax = new int[n];
+        int max = nums[n-1];
+        rmax[n-1] = 0;
+        for(int i = n-2 ; i>= 0 ; i--){
+            rmax[i] = max;
+            max = Math.max(max,nums[i]);
         }
 
-        int pre = 0;
+        int lmax = 0;
         int water = 0;
 
-        for(int i  = 0 ; i<n ; i++){
-            int h = Math.min(pre,nge[i]);
-            int cur = Math.max(0,h-arr[i]);
-            water += cur;
-            pre = Math.max(pre,arr[i]);
+        for(int i = 0 ; i<n ;i++){
+            int h = Math.min(lmax,rmax[i]);
+            water += Math.max(0,h-nums[i]);
+            lmax =Math.max(lmax,nums[i]);
         }
-
-        System.out.print(Arrays.toString(nge));
 
         return water;
     }
