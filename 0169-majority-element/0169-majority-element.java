10ed4e1e120b5d00fc261165fd8b38a591352fe9
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-30 15:27:42 +0530

    Time: 6 ms (51.91%), Space: 56 MB (6.72%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
-        int maxnum = 0;
-        int maxcount = 0;
-
-        for(int i = 0 ; i<n ; i++){
-            if(maxnum != nums[i]){
-                maxcount--;
-                if(maxcount < 0){
-                    maxcount = 1;
-                    maxnum = nums[i];
-                }
-            }else{
-                maxcount++;
-            }
-        }
-
-        return maxnum;
+        Arrays.sort(nums);
+        return nums[n/2];
     }
 }
