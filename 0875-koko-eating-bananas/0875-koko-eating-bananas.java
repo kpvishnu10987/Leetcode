476b5a6869322e1a69a23b00e18995c3c308262a
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-30 17:28:01 +0530

    Time: 6 ms (99.69%), Space: 47.7 MB (74.91%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -5,33 +5,28 @@ class Solution {
         int r = 0;
 
         for(int p : piles){
-            r = Math.max(p,r);
+            r = Math.max(r,p);
         }
-
         while(l < r){
             int mid = l + (r-l)/2;
-
             if(can(mid,piles,h)){
                 r = mid;
             }else{
                 l = mid+1;
             }
         }
+
         return l;
     }
-
     private boolean can(int k,int[] piles,int h){
         int total = 0;
+        int n = piles.length;
 
         for(int p : piles){
-            if(p <= k){
-                total++;
-            }else{
-                total += p/k;
-                if(p%k != 0) total++;
-            }
+            total += (p+k-1)/k;
+            if(total > h) return false;
         }
 
-        return total <= h;
+        return true;
     }
 }
