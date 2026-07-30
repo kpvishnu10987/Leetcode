5ceac7fc73c80e9bea57448b33b76f2cf10f0100
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-07-30 16:20:55 +0530

    Time: 6 ms (87.96%), Space: 44 MB (61.14%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -2,34 +2,26 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
-        if(n>m) return false;
+        if(n > m) return false;
 
         int[] need = new int[26];
         int[] window = new int[26];
 
-        for(char c : s1.toCharArray()){
+        for(char c  : s1.toCharArray()){
             need[c-'a']++;
         }
+        int l = 0;
 
-        for(int i = 0 ; i<m ; i++){
-            window[s2.charAt(i)-'a']++;
+        for(int r = 0 ;  r<m ;r++){
+            window[s2.charAt(r)-'a']++;
 
-            if(i >= n){
-                window[s2.charAt(i-n)-'a']--;
+            if(r-l+1 > n){
+                window[s2.charAt(l)-'a']--;
+                l++;
             }
 
-            if(match(need,window)) return true;
+            if(Arrays.equals(need,window)) return true;
         }
-
         return false;
-
-        
-    }
-
-    private boolean match(int[] a ,int[] b){
-        for(int i = 0 ; i<26 ; i++){
-            if(a[i] != b[i]) return false;
-        }
-        return true;
     }
 }
