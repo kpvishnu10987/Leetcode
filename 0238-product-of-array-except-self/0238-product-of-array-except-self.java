f2e95f48633343d7a274ea239f87c437144c28f7
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-03 22:23:49 +0530

    Time: 2 ms (93.98%), Space: 72 MB (53.21%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -4,20 +4,18 @@ class Solution {
 
         int[] suf = new int[n];
         suf[n-1] = 1;
+
         for(int i = n-2 ; i>=0 ; i--){
-            suf[i] = suf[i+1]*nums[i+1];
+            suf[i] = suf[i+1] * nums[i+1];
         }
 
         int pre = 1;
-        int[] ans = new int[n];
-        
 
         for(int i = 0 ; i<n ; i++){
-            ans[i] = pre * suf[i];
+            suf[i] = pre * suf[i];
             pre = pre * nums[i];
         }
 
-        return ans;
-
+        return suf;
     }
 }
