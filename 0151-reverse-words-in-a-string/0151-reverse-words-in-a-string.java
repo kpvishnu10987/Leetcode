a34fbcac53cf3b41129cae5426335cb18f2fbceb
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-03 22:13:10 +0530

    Time: 4 ms (92.64%), Space: 44.1 MB (87.51%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,30 +1,28 @@
 class Solution {
     public String reverseWords(String s) {
-        s = s.trim(); // remove leading & trailing spaces
-        Stack<String> st = new Stack<>();
+        int n = s.length();
+        int l = 0;
+        
+        List<String> list = new ArrayList<>();
+
+        while(l < n){
+            while(l < n && s.charAt(l) == ' ') l++;
+            if(l >= n) break;
+
+            int end = l;
+            while(end < n && s.charAt(end) != ' ') end++;
+            list.add(s.substring(l,end));
+
+            l = end;
+        }
+        
+
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c != ' ') {
-                sb.append(c);
-            } else if (sb.length() > 0) {
-                // word complete → push and reset
-                st.push(sb.toString());
-                sb.setLength(0);
-                // skip multiple spaces
-                while (i + 1 < s.length() && s.charAt(i + 1) == ' ') i++;
-            }
-        }
-
-        // push the last word
-        if (sb.length() > 0) st.push(sb.toString());
-
-        // build reversed result
-        sb.setLength(0);
-        while (!st.isEmpty()) {
-            sb.append(st.pop());
-            if (!st.isEmpty()) sb.append(' ');
+        for(int i = list.size()-1 ; i>=0 ; i--){
+            if(list.get(i) == " ") continue;
+            sb.append(list.get(i));
+            if(i != 0) sb.append(' ');
         }
 
         return sb.toString();
