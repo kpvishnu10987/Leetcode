73d17aa8fc04627b248a375a8cd8650ce874ef3a
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-04 14:05:37 +0530

    Time: 3 ms (87.47%), Space: 52.3 MB (46.96%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,48 +1,39 @@
 class Solution {
+    char[][] grid;
+    int n;
+    int m;
     public int numIslands(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+        this.grid = grid;
+        n = grid.length;
+        m = grid[0].length;
 
-        boolean[][] vis = new boolean[n][m];
 
-        int[] drow = {0,1,0,-1};
-        int[] dcol ={1,0,-1,0};
+        
         int comp = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1' && !vis[i][j]) {
-                    vis[i][j] = true;
+                if(grid[i][j] == '1'){
                     comp++;
-                    Queue<int[]> q = new LinkedList<>();
-                    q.offer(new int[] { i, j });
-
-                    while (!q.isEmpty()) {
-                        int size = q.size();
-                        for(int k = 0 ; k<size; k++){
-
-                        
-
-                        int[] cur = q.poll();
-                        int r = cur[0];
-                        int c = cur[1];
-
-                        for (int a = 0; a < 4; a++) {
-                            int nr = r + drow[a];
-                            int nc = c + dcol[a];
-
-                            if (nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == '1' && !vis[nr][nc]) {
-                                vis[nr][nc] = true;
-                                q.offer(new int[] { nr, nc });
-                            }
-                        }
-                        }
-                    }
+                    dfs(i,j);
                 }
             }
         }
 
         return comp;
 
+    }
+
+    private void dfs(int i,int j){
+        if(i < 0 || j < 0 || i > n-1 || j > m-1) return;
+
+        if(grid[i][j] == '0') return;
+
+        grid[i][j] = '0';
+
+        dfs(i+1,j);
+        dfs(i,j+1);
+        dfs(i-1,j);
+        dfs(i,j-1); 
     }
 }
