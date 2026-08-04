d96f236aa528508c7c52308d2c5500f2d12c2c14
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-04 13:58:27 +0530

    Time: 8 ms (9.75%), Space: 51.4 MB (89.45%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -3,43 +3,41 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
 
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][] visited = new boolean[n][m];
-        int cnt  = 0;
-        for(int i = 0 ; i<n ; i++){
-            for(int j = 0 ; j<m ; j++){
-                if(!visited[i][j] && grid[i][j] == '1'){
-                    cnt++;
-                    q.offer(new int[]{i,j});
-                    bfs(q,visited,grid);
+        boolean[][] vis = new boolean[n][m];
+
+        int[] drow = {0,1,0,-1};
+        int[] dcol ={1,0,-1,0};
+        int comp = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1' && !vis[i][j]) {
+                    vis[i][j] = true;
+                    comp++;
+                    Queue<int[]> q = new LinkedList<>();
+                    q.offer(new int[] { i, j });
+
+                    while (!q.isEmpty()) {
+
+                        int[] cur = q.poll();
+                        int r = cur[0];
+                        int c = cur[1];
+
+                        for (int a = 0; a < 4; a++) {
+                            int nr = r + drow[a];
+                            int nc = c + dcol[a];
+
+                            if (nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == '1' && !vis[nr][nc]) {
+                                vis[nr][nc] = true;
+                                q.offer(new int[] { nr, nc });
+                            }
+                        }
+                    }
                 }
             }
         }
-        return cnt;
-    }
-    private void bfs(Queue<int[]> q,boolean[][] visited,char[][] grid){
-        int n = grid.length;
-        int m = grid[0].length;
 
-        int[] drow = {-1,0,+1,0};
-        int[] dcol = {0,+1,0,-1};
-
-        while(!q.isEmpty()){
-            int[] p = q.poll();
-            for(int i = 0 ; i<4 ;i++){
-                int nr = p[0] + drow[i];
-            int nc = p[1] + dcol[i];
-
-            if(nr>=0 && nr<n && nc >=0 && nc <m && !visited[nr][nc]){
-                visited[nr][nc] = true;
-                if(grid[nr][nc] == '1'){
-                    q.offer(new int[]{nr,nc});
-                }
-            }
-            }
-        }
-
-
+        return comp;
 
     }
 }
