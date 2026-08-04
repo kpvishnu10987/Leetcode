42238622a473443466b53373c58bd753b0398000
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-04 15:12:35 +0530

    Time: 30 ms (7.38%), Space: 44.3 MB (41.11%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -21,16 +21,44 @@ class Node {
 class Solution {
     Map<Node,Node> map = new HashMap<>();
     public Node cloneGraph(Node node) {
-        if(map.containsKey(node)) return map.get(node);
         if(node == null) return null;
+        Queue<Node> q = new LinkedList<>();
+        q.offer(node);
 
-        Node clone = new Node(node.val);
-        map.put(node,clone);
-        for(Node nei : node.neighbors){
-            clone.neighbors.add(cloneGraph(nei));
+        while(!q.isEmpty()){
+            Node cur = q.poll();
+
+            Node clone = new Node(cur.val);
+            map.put(cur,clone);
+
+            for(Node nei : cur.neighbors){
+                if(!map.containsKey(nei)){
+                    q.offer(nei);
+                }
+            }
         }
 
-        return clone;
-        
+        q.offer(node);
+        Set<Node> vis = new HashSet<>();
+        vis.add(node);
+
+        while(!q.isEmpty()){
+            Node cur = q.poll();
+            Node clone = map.get(cur);
+            
+
+            for(Node nei: cur.neighbors){
+                clone.neighbors.add(map.get(nei));
+                if(!vis.contains(nei)){
+                    vis.add(nei);
+                    q.offer(nei);
+                }
+                
+            }
+        }
+
+
+
+        return map.get(node);
     }
 }
