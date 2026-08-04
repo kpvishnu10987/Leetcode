35c6eca23fcd8e8571dc0d27e582c98744d98656
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-04 17:37:46 +0530

    Time: 1 ms (91.3%), Space: 45.2 MB (38.54%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,42 +1,47 @@
 class Solution {
-    int[] parent;
+    int par[];
+    int size[];
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
-
-        parent = new int[n+1];
-
-        for(int i = 0 ; i<=n ;i++){
-            parent[i] = i;
+        par = new int[n+1];
+        size = new int[n+1];
+        for(int i = 0 ; i<n;  i++){
+            par[i] = i;
+            size[i] = 1;
         }
 
-        for(int[] edge : edges){
-            int u = edge[0];
-            int v = edge[1];
+        for(int[] e: edges){
+            int a = e[0];
+            int b = e[1];
 
-            if(find(u) == find(v)){
-                return edge;
-            }
+            if(find(a) == find(b)) return e;
 
-            union(u,v);
+            union(a,b);
         }
+
 
         return new int[0];
     }
 
     private int find(int x){
-        if(parent[x] == x){
-            return x;
-        }
+        if(x == par[x]) return x;
 
-        return parent[x] = find(parent[x]);
+        return par[x]= find(par[x]);
     }
 
     private void union(int u,int v){
-        int pu = parent[u];
-        int pv = parent[v];
+        int pu = find(u);
+        int pv = find(v);
 
-        if(pu != pv){
-            parent[pu] = pv;
+        if(pu == pv) return;
+
+        if(size[pu] > size[pv]){
+            par[pv] = pu;
+            size[pu] += size[pv];
+        }else{
+            par[pu] = pv;
+            size[pv] += size[pu];
         }
     }
 }
+
