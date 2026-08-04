02b60d131d63b8114423c57ca09b92dd3e70dc07
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-04 16:57:24 +0530

    Time: 5 ms (53.19%), Space: 47.3 MB (73.84%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,27 +1,30 @@
 class Solution {
-    int[] drow = {-1,1,0,0};
-    int[] dcol = {0,0,-1,1};
-    public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        int n = heights.length;
-        int m = heights[0].length;
+    int[][] arr;
+    int n;
+    int m;
+    public List<List<Integer>> pacificAtlantic(int[][] arr) {
+        List<List<Integer>> ans = new ArrayList<>();
+        this.arr = arr;
+        n = arr.length;
+        m = arr[0].length;
 
+        boolean[][] pac = new boolean[n][m];
         boolean[][] at = new boolean[n][m];
-        boolean[][] pa = new boolean[n][m];
 
         for(int i = 0 ; i<n ; i++){
-            dfs(i,0,heights,pa);
-            dfs(i,m-1,heights,at);
+            f(i,0,pac);
+            f(i,m-1,at);
         }
 
         for(int i = 0 ; i<m ; i++){
-            dfs(0,i,heights,pa);
-            dfs(n-1,i,heights,at);
+            f(0,i,pac);
+            f(n-1,i,at);
         }
 
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i = 0 ; i<n ; i++){
+
+        for(int i = 0 ; i< n ; i++){
             for(int j = 0 ; j<m ; j++){
-                if(pa[i][j] && at[i][j]){
+                if(pac[i][j] && at[i][j]){
                     ans.add(new ArrayList<>(List.of(i,j)));
                 }
             }
@@ -30,16 +33,18 @@ class Solution {
         return ans;
     }
 
-    private void dfs(int i,int j,int[][] grid,boolean[][] vis){
-        if(vis[i][j]) return;
+    private void f(int i,int j,boolean[][] vis){
         vis[i][j] = true;
+        int[] dr = {0,1,-1,0};
+        int[] dc = {-1,0,0,1};
 
         for(int k = 0 ; k<4 ; k++){
-            int r = i + drow[k];
-            int c = j + dcol[k];
-            if(r >=0 && r <grid.length && c >=0  && c < grid[0].length && vis[r][c] == false){
-                if(grid[r][c] >= grid[i][j]){
-                    dfs(r,c,grid,vis);
+            int nr = i + dr[k];
+            int nc = j + dc[k];
+
+            if(nr >= 0 && nc >=0 && nr < n && nc <m && !vis[nr][nc]){
+                if(arr[nr][nc] >= arr[i][j]){
+                    f(nr,nc,vis);
                 }
             }
         }
