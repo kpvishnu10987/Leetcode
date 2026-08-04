a51c29783e1d3f4e6ff39abb4f4f0e9f5e4def6d
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-04 17:10:33 +0530

    Time: 6 ms (72.01%), Space: 47.4 MB (13.17%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,30 +1,28 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] arr) {
-        int n = numCourses;
-        int in[] = new int[n];
-        Stack<Integer> st = new Stack<>();
-
+    public boolean canFinish(int n, int[][] arr) {
         List<List<Integer>> graph = new ArrayList<>();
+        int[] in = new int[n];
+
+        Stack<Integer> st = new Stack<>();
 
         for(int i = 0 ; i<n ; i++){
             graph.add(new ArrayList<>());
         }
 
-        for(int[] p : arr){
-            int a = p[0];
-            int b = p[1];
-            graph.get(b).add(a);
-            in[a]++;
+        for(int[] a : arr){
+            graph.get(a[1]).add(a[0]);
+            in[a[0]]++;
         }
 
         for(int i = 0 ; i<n ; i++){
-            if(in[i] == 0) st.push(i);
+            if(in[i] == 0){
+                st.push(i);
+            }
         }
 
         while(!st.isEmpty()){
-            int node = st.pop();
-
-            for(int nei : graph.get(node)){
+            int cur = st.pop();
+            for(int nei : graph.get(cur)){
                 in[nei]--;
                 if(in[nei] == 0){
                     st.push(nei);
@@ -37,7 +35,5 @@ class Solution {
         }
 
         return true;
-
-
     }
 }
