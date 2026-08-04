af70fb4b7a76552f5755162cb5986bffb4e131cb
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-04 21:19:46 +0530

    Time: 6 ms (61.29%), Space: 42.7 MB (91.79%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,32 +1,18 @@
 class Solution {
+    int ans = 0;
     public int countSubstrings(String s) {
-        int n = s.length();
-        boolean dp[][] = new boolean[n][n];
-        int count = 0;
-
-        for(int len = 1 ; len <= n ; len++){
-            for(int i = 0 ; i<= n-len ; i++){
-                int j = i+len-1;
-
-                if(len == 1){
-                    count++;
-                    dp[i][j] = true;
-                }
-                if(len == 2 && s.charAt(i) == s.charAt(j)){
-                    count++;
-                    dp[i][j] = true;
-                }
-                if(len > 2 && dp[i+1][j-1]){
-                    if(s.charAt(i) == s.charAt(j)){
-                        count++;
-                        dp[i][j] = true;
-                    }
-                }
-            }
+        for(int i = 0 ; i<s.length() ; i++){
+            expand(s,i,i);
+            expand(s,i,i+1);
         }
-
-        return count;
+        return ans;
     }
 
-   
+    private void expand(String s,int l,int r){
+        while(l >=0 && r < s.length() && s.charAt(l) == s.charAt(r)){
+            ans++;
+            l--;
+            r++;
+        }
+    }
 }
