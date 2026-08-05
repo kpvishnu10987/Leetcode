c6be62cea0ebf7b40769900b942521d2a29da84a
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-05 22:26:09 +0530

    Time: 9 ms (31.76%), Space: 44.2 MB (15.31%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -1,37 +1,25 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
+        int n = s.length();
         Map<Character,Integer> last = new HashMap<>();
-        Map<Character,Integer> first = new HashMap<>();
 
         for(int i = 0 ;i<s.length() ; i++){
             char c = s.charAt(i);
-            if(!first.containsKey(c)) first.put(c,i);
             last.put(c,i);
         }
 
-        List<int[]> intervals = new ArrayList<>();
-        for(char c : s.toCharArray()){
-            intervals.add(new int[]{first.get(c),last.get(c)});
-        }
-        intervals.sort((a,b)->a[0]-b[0]);
-        List<int[]> ans = new ArrayList<>();
-        int[] prev = intervals.get(0);
+        int end = 0;
+        int start = 0 ;
+        List<Integer> ans = new ArrayList<>();
 
-        for(int i = 1 ;i<intervals.size() ; i++){
-            if(prev[1] < intervals.get(i)[0]){
-                ans.add(prev);
-                prev = intervals.get(i);
-            }else{
-                prev[1] = Math.max(prev[1],intervals.get(i)[1]);
+        for(int i = 0 ; i<n ;i++){
+            end = Math.max(end,last.get(s.charAt(i)));
+            if(i == end){
+                ans.add(end-start+1);
+                start = end+1;
             }
         }
-        ans.add(prev);
-
-        List<Integer> res = new ArrayList<>();
-        for(int i = 0 ;i<ans.size() ; i++){
-            res.add(ans.get(i)[1]-ans.get(i)[0]+1);
-        }
-
-        return res;
+        return ans;
+   
     }
 }
