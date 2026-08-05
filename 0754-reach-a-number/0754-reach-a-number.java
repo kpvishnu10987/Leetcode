adb945dd181e491f70ab81663bdf86b37183d25e
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-05 21:54:14 +0530

    Time: 2 ms (70.13%), Space: 42.2 MB (38.96%) - LeetHub

---
+++ 0754-reach-a-number/0754-reach-a-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reachNumber(int target) {
+        target = Math.abs(target);
+        int steps = 0;
+        int sum = 0;
+
+        while(sum < target || (sum - target) % 2 != 0){
+            steps++;
+            sum += steps;
+        }
+
+        return steps;
+    }
+}
