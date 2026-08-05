b73988c17ad9f19ad93e15a26e5757c6bea3a64e
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-05 08:38:41 +0530

    Time: 93 ms (22.51%), Space: 72.5 MB (31.35%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,41 +1,33 @@
 class Solution {
+    int[] nums;
     int n;
     Boolean[][] dp;
-    int[] nums;
     public boolean canPartition(int[] nums) {
-        this.nums = nums;
         n = nums.length;
-      
+        this.nums = nums;
 
         int sum = 0;
-        for(int i = 0 ; i<n ;i++){
-            sum += nums[i];
-        }
+        for(int num : nums) sum += num;
 
         if(sum % 2 != 0) return false;
 
         sum /= 2;
+
         dp = new Boolean[n][sum+1];
 
         return f(0,sum);
     }
 
-    private boolean f(int i,int k){
-        if(i == n){
-            if(k == 0) return true;
-            return false;
-        }
-        if(k < 0) return false;
+    private boolean f(int i,int target){
+        if(target == 0) return true;
+        if(i >= n) return false;
+        if(target < 0) return false;
 
-        if(dp[i][k] != null) return dp[i][k]; 
+        if(dp[i][target] != null) return dp[i][target];
 
+        boolean t = f(i+1,target-nums[i]);
+        boolean nt = f(i+1,target);
 
-        boolean ans = false;
-
-
-        ans |= f(i+1,k-nums[i]);
-        ans |= f(i+1,k);
-
-        return dp[i][k] =  ans;
+        return dp[i][target] = t || nt;
     }
 }
