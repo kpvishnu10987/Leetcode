a56bd86d8a435a321e1b27e6b2588f5ef9cee720
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-05 22:35:04 +0530

    Time: 1 ms (58.93%), Space: 47.8 MB (56.9%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,22 +1,21 @@
 class Solution {
-    public int trap(int[] nums) {
-        int n = nums.length;
+    public int trap(int[] arr) {
+        int n = arr.length;
 
         int[] rmax = new int[n];
-        int max = nums[n-1];
         rmax[n-1] = 0;
-        for(int i = n-2 ; i>= 0 ; i--){
-            rmax[i] = max;
-            max = Math.max(max,nums[i]);
-        }
+
+        for(int i = n-2 ; i>=0 ; i--){
+            rmax[i] = Math.max(rmax[i+1],arr[i+1]);
+        } 
 
         int lmax = 0;
         int water = 0;
 
-        for(int i = 0 ; i<n ;i++){
+        for(int i = 0 ; i<n ; i++){
             int h = Math.min(lmax,rmax[i]);
-            water += Math.max(0,h-nums[i]);
-            lmax =Math.max(lmax,nums[i]);
+            water += Math.max(0,h-arr[i]);
+            lmax = Math.max(lmax,arr[i]);
         }
 
         return water;
