0676ea36c74ae665bb9f47e0d64db1b9773b9aed
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-05 11:13:11 +0530

    Time: 29 ms (17.05%), Space: 57.3 MB (16.7%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,15 +1,26 @@
 class Solution {
+    int[] coins;
+    int[][] dp;
+    int n;
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int[] dp = new int[amount+1];
-        dp[0] = 1;
+        this.coins = coins;
+        n = coins.length;
+        dp = new int[n][amount+1];
+        for(int[] r : dp) Arrays.fill(r,-1);
 
-        for(int coin : coins){
-            for(int i = coin ; i<= amount ; i++){
-                dp[i] += dp[i-coin];
-            }
-        }
+        return f(0,amount);
+    }
 
-        return dp[amount];
+    private int f(int i,int amount){
+        if(amount == 0) return 1;
+        if(i == n || amount < 0) return 0;
+
+        if(dp[i][amount] != -1) return dp[i][amount];
+
+        int ans = 0;
+
+        
+
+        return dp[i][amount] = f(i+1,amount) + f(i,amount-coins[i]);
     }
 }
