0267930270619409a51a5faee16a6e4654b52990
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-05 11:42:29 +0530

    Time: 0 ms (100%), Space: 43 MB (81.37%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -0,0 +1,50 @@
+class Solution {
+    String s1;
+    String s2;
+    String s3;
+    int n;
+    int m;
+    Boolean[][] dp;
+    public boolean isInterleave(String s1, String s2, String s3) {
+        this.s1 = s1;
+        this.s2 = s2;
+        this.s3 = s3;
+        
+        n = s1.length();
+        m = s2.length();
+        if(n + m != s3.length()) return false;
+
+        dp = new Boolean[n][m];
+
+        return f(0,0);
+        
+    }
+
+    private boolean f(int i,int j){
+        if(i == n && j == m) return true;
+        if(i == n){
+            return s2.substring(j).equals(s3.substring(i+j));
+        }
+        if(j == m){
+            return s1.substring(i).equals(s3.substring(i+j));
+        }
+
+        if(dp[i][j] != null) return dp[i][j];
+
+        int a = s1.charAt(i);
+        int b = s2.charAt(j);
+        int c= s3.charAt(i+j);
+        if(a != c && b != c) return false;
+        boolean ans = false;
+
+        if(a == c && b == c){
+            ans |= f(i+1,j) || f(i,j+1);
+        }else if(a == c){
+            ans |= f(i+1,j);
+        }else if(b == c){
+            ans |= f(i,j+1);
+        }
+
+        return dp[i][j] = ans;
+    }
+}
