a8ea0db3b7a3efc8adea5fcd54c53124028f63ea
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-08 21:51:35 +0530

    Time: 36 ms (30.25%), Space: 59.3 MB (45.86%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -6,25 +6,31 @@ class Solution {
 
         for(int i = 0 ; i<n ; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
-            int l = i+1;
+            int a = nums[i];
+
+            int l = i+1 ;
             int r = n-1;
 
             while(l < r){
-                int sum = nums[l] + nums[r] + nums[i];
+                int b = nums[l];
+                int c = nums[r];
+                int sum = a+ b+ c;
                 if(sum == 0){
-                    ans.add(new ArrayList<>(List.of(nums[i],nums[l],nums[r])));
+                    ans.add(new ArrayList<>(List.of(a,b,c)));
+
                     l++;
                     r--;
                     while(l < r && nums[l] == nums[l-1]) l++;
                     while(l < r && nums[r] == nums[r+1]) r--;
-                }else if(sum > 0){
-                    r--;
-                }else{
+                }else if(sum < 0){
                     l++;
+                }else{
+                    r--;
                 }
             }
-        }
 
+        }
         return ans;
+        
     }
 }
