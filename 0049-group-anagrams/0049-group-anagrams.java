479473e6f8f0ccffa55213dc4cd268bc9e40bdf1
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-08 23:43:21 +0530

    Time: 8 ms (33.98%), Space: 49.9 MB (25.03%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,27 +1,17 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> ans = new ArrayList<>();
+
         Map<String,List<String>> map = new HashMap<>();
 
         for(String s : strs){
-            int[] freq = new int[26];
-            for(int i = 0 ; i<s.length() ; i++){
-                freq[s.charAt(i)-'a']++;
-            }
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
 
-            StringBuilder sb = new StringBuilder();
-
-            for(int x : freq){
-                sb.append(x).append('&');
-            }
-            String key = sb.toString();
-
-            if(!map.containsKey(key)) map.put(key,new ArrayList<>());
-            List<String> sub = map.get(key);
-            sub.add(s);
-            map.put(key,sub);
+            map.putIfAbsent(key,new ArrayList<>());
+            map.get(key).add(s);
         }
-
         for(String key : map.keySet()){
             ans.add(map.get(key));
         }
