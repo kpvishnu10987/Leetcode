9eead22d3e8d9d445c377e4fae4c78e1120561a4
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-09 13:14:36 +0530

    Time: 67 ms (83.59%), Space: 48 MB (59.16%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,37 +1,33 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> set = new HashSet<>(wordList);
+        if(!set.contains(endWord)) return 0;
 
-        Queue<String> q = new LinkedList<>();
-
-        if (!set.contains(endWord))
-            return 0;
-
+        Queue<String> q =  new LinkedList<>();
         q.offer(beginWord);
-        int len = 1;
+        int level = 1;
 
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             int size = q.size();
-            len++;
-            while (size-- > 0) {
-                String word = q.poll();
-                char[] arr = word.toCharArray();
-
-                for (int i = 0; i < arr.length; i++) {
-                    char ori = arr[i];
-                    for (int j = 'a'; j <= 'z'; j++) {
-                        arr[i] = (char) j;
-                        String newword = new String(arr);
-                        if (newword.equals(endWord))
-                            return len;
-                        if (set.contains(newword)) {
-                            set.remove(newword);
-                            q.offer(newword);
+            for(int i = 0 ;i<size ;i++){
+                String cur = q.poll();
+                if(cur.equals(endWord)) return level;
+                char[] arr = cur.toCharArray();
+                for(int j = 0; j<arr.length; j++){
+                    char ori = arr[j];
+                    for(char c = 'a' ; c <= 'z' ; c++){
+                        arr[j] = c;
+                        String next = new String(arr);
+                        if(set.contains(next)){
+                            q.offer(next);
+                            set.remove(next);
                         }
                     }
-                    arr[i] = ori;
+                    arr[j] = ori;
                 }
+
             }
+            level++;
         }
 
         return 0;
