73e722a427d88f103fe2f053cdd886e6ff724b0f
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-09 11:49:57 +0530

    Time: 1 ms (38.42%), Space: 46.8 MB (44.09%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -3,28 +3,29 @@ class Solution {
         int n = image.length;
         int m = image[0].length;
 
+        int ori = image[sr][sc];
+        if(color == ori) return image;
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{sr,sc});
-        int co = image[sr][sc];
-        int[][] visited = new int[n][m];
-        int drow[] = {-1,0,+1,0};
-        int dcol[] = {0,+1,0,-1};
         image[sr][sc] = color;
+        q.offer(new int[]{sr,sc});
+
+        int[] drow = {0,0,1,-1};
+        int[] dcol = {1,-1,0,0};
 
         while(!q.isEmpty()){
-            int[] point = q.poll();
-            for(int i = 0 ;i<4 ;i++){
-                int r = point[0] + drow[i];
-                int c= point[1] + dcol[i];
-                if(r >= 0 && r<n && c >=0 && c <m && visited[r][c] != 1 && image[r][c] == co ){
-                    visited[r][c] = 1;
-                    image[r][c] = color;
-                    q.offer(new int[]{r,c});
+            int[] node = q.poll();
+            int r = node[0];
+            int c = node[1];
+            for(int i = 0 ; i<4 ;i++){
+                int nr = r + drow[i];
+                int nc = c + dcol[i];
+                if(nr >= 0&& nc >=0 && nr < n&& nc <m && image[nr][nc] == ori){
+                    image[nr][nc] = color;
+                    q.offer(new int[]{nr,nc});
                 }
             }
         }
 
         return image;
-        
     }
 }
