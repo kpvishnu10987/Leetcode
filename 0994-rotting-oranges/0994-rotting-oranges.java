0968f55e869cf1650b5c345aef0bd1ef7138df67
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-09 12:27:01 +0530

    Time: 2 ms (89.91%), Space: 44 MB (85.44%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -3,52 +3,48 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
 
-        Queue<int[]> q = new LinkedList<>();
-
         int fresh = 0;
         int rotten = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    q.offer(new int[] { i, j });
+        Queue<int[]> q = new LinkedList<>();
+        for(int i = 0; i<n ;i++){
+            for(int j = 0 ;j<m ;j++){
+                if(grid[i][j] == 2){
+                    q.offer(new int[]{i,j});
                     rotten++;
-                } else if (grid[i][j] == 1) {
+                }else if(grid[i][j] == 1){
                     fresh++;
                 }
             }
         }
-        if (fresh == 0)
-            return 0;
-        if (rotten == 0)
-            return -1;
+        if(rotten == 0 && fresh != 0) return -1;
+        if(fresh == 0) return 0;
+        int drow[] = {0,0,1,-1};
+        int dcol[] = {1,-1,0,0};
         int time = 0;
-        int[] drow = { 0, 1, -1, 0 };
-        int[] dcol = { 1, 0, 0, -1 };
 
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             int size = q.size();
-            for (int k = 0; k < size; k++) {
-                int[] cur = q.poll();
-                int r = cur[0];
-                int c = cur[1];
-
-                for (int i = 0; i < 4; i++) {
-                    int nr = drow[i] + r;
-                    int nc = dcol[i] + c;
-
-                    if (nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1) {
-                        grid[nr][nc] = 2;
-                        q.offer(new int[] { nr, nc });
-                        fresh--;
+            if(fresh == 0) return time;
+            while(size-- > 0){
+                int[] node = q.poll();
+                int r = node[0];
+                int c = node[1];
+                for(int i = 0 ; i<4 ; i++){
+                    int nr = r + drow[i];
+                    int nc = c + dcol[i];
+                    if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1){
+                      grid[nr][nc] = 2;
+                      fresh--;
+                      q.offer(new int[]{nr,nc});
                     }
                 }
             }
-            ++time;
+            time++;
         }
 
-        if (fresh > 0)
-            return -1;
+        if(fresh != 0) return -1;
+        return time;
 
-        return time-1;
+
     }
 }
