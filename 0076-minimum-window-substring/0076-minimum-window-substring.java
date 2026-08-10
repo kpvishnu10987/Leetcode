e5c370c10e2d8976e496be71d45df02fc8801ae2
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-10 16:12:54 +0530

    Time: 337 ms (5%), Space: 47.1 MB (46.27%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -2,46 +2,42 @@ class Solution {
     public String minWindow(String s, String t) {
         int n = s.length();
         int m = t.length();
+        if(m > n) return "";
 
-        if(n < m) return new String();
-        
-
-        int tcount = 0;
         Map<Character,Integer> smap = new HashMap<>();
         Map<Character,Integer> tmap = new HashMap<>();
-
         for(char c : t.toCharArray()){
             tmap.put(c,tmap.getOrDefault(c,0)+1);
-            tcount++;
         }
-        int l= 0;
+
+        int l = 0;
         int min = Integer.MAX_VALUE;
         int start = 0;
-
-        for(int r = 0 ; r<n ; r++){
+        for(int r = 0 ; r< n ; r++){
             char rc = s.charAt(r);
             smap.put(rc,smap.getOrDefault(rc,0)+1);
-            if(tmap.containsKey(rc) && smap.get(rc) <= tmap.get(rc)) tcount--;
 
-            while(tcount == 0){
-                char lc = s.charAt(l);
+            while(valid(smap,tmap)){
+                
                 if(min > r-l+1){
                     start = l;
                     min = r-l+1;
                 }
-                int count = smap.get(lc);
-                if(count == 1){
-                    smap.remove(lc);
-                }else{
-                    smap.put(lc,smap.get(lc)-1);
-                }
-                 if(tmap.containsKey(lc) && tmap.get(lc) > smap.getOrDefault(lc,0)){
-                        tcount++;
-                    }
+                char lc = s.charAt(l);
+                smap.put(lc,smap.get(lc)-1);
                 l++;
             }
-
+            
         }
-            return min == Integer.MAX_VALUE ? "" :s.substring(start,start+min);
+
+        return min == Integer.MAX_VALUE ? "" : s.substring(start,start+min);
+    }
+    boolean valid(Map<Character,Integer> smap,Map<Character,Integer> tmap){
+        for(char key : tmap.keySet()){
+            if(!smap.containsKey(key)) return false;
+            if(smap.get(key) < tmap.get(key)) return false;
+        }
+
+        return true;
     }
 }
