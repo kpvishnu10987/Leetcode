7a30d0b08a0039419a66c3eec0ba661ff88545d4
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-08-10 16:26:40 +0530

    Time: 19 ms (41.3%), Space: 47.7 MB (23.71%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -2,42 +2,42 @@ class Solution {
     public String minWindow(String s, String t) {
         int n = s.length();
         int m = t.length();
-        if(m > n) return "";
 
+        if(m > n) return "";
         Map<Character,Integer> smap = new HashMap<>();
         Map<Character,Integer> tmap = new HashMap<>();
+
+        int need = m;
+        int start = 0;
+        int min = Integer.MAX_VALUE;
+
         for(char c : t.toCharArray()){
             tmap.put(c,tmap.getOrDefault(c,0)+1);
         }
 
         int l = 0;
-        int min = Integer.MAX_VALUE;
-        int start = 0;
-        for(int r = 0 ; r< n ; r++){
+        for(int r = 0 ; r<n ; r++){
             char rc = s.charAt(r);
             smap.put(rc,smap.getOrDefault(rc,0)+1);
 
-            while(valid(smap,tmap)){
-                
+            if(tmap.containsKey(rc) && tmap.get(rc) >= smap.get(rc)){
+                need--;
+            }
+
+            while(need == 0){
                 if(min > r-l+1){
                     start = l;
                     min = r-l+1;
                 }
+
                 char lc = s.charAt(l);
+                if(tmap.containsKey(lc) && tmap.get(lc) >= smap.get(lc)){
+                    need++;
+                }
                 smap.put(lc,smap.get(lc)-1);
                 l++;
             }
-            
         }
-
-        return min == Integer.MAX_VALUE ? "" : s.substring(start,start+min);
-    }
-    boolean valid(Map<Character,Integer> smap,Map<Character,Integer> tmap){
-        for(char key : tmap.keySet()){
-            if(!smap.containsKey(key)) return false;
-            if(smap.get(key) < tmap.get(key)) return false;
-        }
-
-        return true;
+        return min == Integer.MAX_VALUE?"":s.substring(start,start+min);
     }
 }
