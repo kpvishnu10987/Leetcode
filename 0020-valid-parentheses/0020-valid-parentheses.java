5584562185ef32ff70de6e7ca9637455e2715277
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-09-15 09:37:38 +0530

    Time: 2 ms (97.94%), Space: 43.7 MB (7.19%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
+        int n = s.length();
         Stack<Character> st = new Stack<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            if (c == '(' || c == '{' || c == '[') {
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
                 st.push(c);
-            } else if (
-                !st.isEmpty() &&
-                (
-                    (st.peek() == '(' && c == ')') ||
-                    (st.peek() == '{' && c == '}') ||
-                    (st.peek() == '[' && c == ']')
-                )
-            ) {
-                st.pop();
-            } else {
-                return false;
+            }else{
+                if(st.isEmpty()) return false;
+                if(c == ')' && st.peek() == '('){
+                    st.pop();
+                }else if(c == ']' && st.peek() == '['){
+                    st.pop();
+                }else if(c == '}' && st.peek() == '{'){
+                    st.pop();
+                }else{
+                    return false;
+                }
             }
         }
+
         return st.isEmpty();
     }
 }
