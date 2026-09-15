0f6fc05836f0da5556968c3959fdc5f700bd3d69
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-09-15 09:43:41 +0530

    Time: 80 ms (8.23%), Space: 102.1 MB (96.15%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,33 +1,31 @@
 class Solution {
-    public int[] dailyTemperatures(int[] temperatures) {
-        int n = temperatures.length;
-        int[] ng = new int[n];
+    public int[] dailyTemperatures(int[] arr) {
+        int n = arr.length;
+
+        int[] nge = new int[n];
         Stack<Integer> st = new Stack<>();
-        for(int i = n-1 ;i>=0 ; i--){
-            if(st.isEmpty()){
-                ng[i] = i;
-                st.push(i);
-            }else{
-                while(!st.isEmpty() && (temperatures[i] >= temperatures[st.peek()])){
-                    st.pop();
-                }
-
-                if(st.isEmpty()){
-                    ng[i] = i;
-                    st.push(i);
-                }else{
-                    ng[i] = st.peek();
-                    st.push(i);
-                }
+        for(int i = n-1 ; i>=0 ; i--){
+            while(!st.isEmpty() && arr[st.peek()] <= arr[i]){
+                st.pop();
             }
+            
+            if(st.isEmpty()){
+                nge[i] = n;
+            }else{
+                nge[i] = st.peek();
+            }
+
+            st.push(i);
         }
 
-        int[] ans = new  int[n];
-        for(int i =0 ; i<n ; i++){
-            ans[i] = ng[i] - i;
+        int[] ans = new int[n];
+        for(int i = 0 ; i<n ; i++){
+            if(nge[i] == n){
+                ans[i] = 0;
+                continue;
+            }
+            ans[i] = nge[i] - i;
         }
-
         return ans;
-        
     }
 }
