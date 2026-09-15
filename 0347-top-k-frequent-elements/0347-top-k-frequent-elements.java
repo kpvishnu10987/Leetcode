bdb71efd55a1ada54c91e802a6c0811b62898914
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-09-15 06:21:02 +0530

    Time: 14 ms (69.9%), Space: 47.7 MB (49.65%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,38 +1,26 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int[] ans = new int[k];
         int n = nums.length;
-
-        Map<Integer,Integer> map = new HashMap<>();
+        Map<Integer,Integer> fmap = new HashMap<>(); //num -> freq
+        PriorityQueue<int[]> map = new PriorityQueue<>((a,b)->a[0]-b[0]); // freq -> list of num
+        
         for(int num : nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+            fmap.put(num,fmap.getOrDefault(num,0) + 1);
         }
 
-
-        List<Integer>[] bucket = new List[n+1];
-
-        for(int key : map.keySet()){
-            int freq = map.get(key);
-            if(bucket[freq] == null){
-                bucket[freq] = new ArrayList<>();
-            }
-
-            bucket[freq].add(key);
-            
+        for(int num : fmap.keySet()){
+            int freq = fmap.get(num);
+            map.offer(new int[]{freq,num});
+            if(map.size() > k) map.poll();
         }
 
-        int idx  = 0;
-
-        for(int i = n ; i>= 1 ; i--){
-            if(bucket[i] == null) continue;
-            if(idx == k) break;
-
-            List<Integer> ele = bucket[i];
-            for(int num : ele){
-                ans[idx++] = num;
-                if(idx == k) break;
-            }
+        int[] ans = new int[k];
+        int j = 0;
+        while(!map.isEmpty()){
+            ans[j++] = map.poll()[1];
         }
         return ans;
+
+
     }
 }
