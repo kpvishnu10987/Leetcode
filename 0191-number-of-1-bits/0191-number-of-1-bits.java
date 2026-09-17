7decbefa2ebeeeaedb0b52bf2e619edc80cb292f
Kuruba Pujari Vishnu Sai Vardhan <155976531+kpvishnu10987@users.noreply.github.com>
2026-09-17 07:14:21 +0530

    Time: 0 ms (100%), Space: 42.6 MB (11.68%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,10 +1,9 @@
 class Solution {
     public int hammingWeight(int n) {
         int c =0;
-        int i = 0;
-        while(n >> i > 0){
-            c += (n >> i & 1);
-            i++;
+        while(n != 0){
+            c += (n & 1);
+            n = n >>1;
         }
         return c;
     }
